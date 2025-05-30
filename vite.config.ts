2e2141c60d646b7b1b3605088699ec5be8ebb075
Andrey Solovyev <andrewsl@yandex.ru>
2025-05-30 15:24:03 +0300

    Fixed module settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    minify: true,
     lib: {
       entry: './src/index.ts',
       name: 'vue-imagine',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        }
+      }
     },
   },
   plugins: [
